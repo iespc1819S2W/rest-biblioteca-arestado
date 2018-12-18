ed5dcf1f06f25a7d8806b54728dbec6aa0bd8d07
ayoubdaw <ayoubbouyssfdaouan@iespaucasesnoves.cat>
2018-12-18 20:21:04 +0100

    Creat totsLlibres.php i mostra tots els llibres

---
+++ totsLlibres.php
@@ -0,0 +1,9 @@
+<?php
+// get autor
+ $base = __DIR__;
+ require_once("$base/model/llibres.class.php");
+ $llibre=new llibre();
+ $res=$llibre->getAll();
+ header('Content-type: application/json');
+ echo json_encode($res);
+ ?>
